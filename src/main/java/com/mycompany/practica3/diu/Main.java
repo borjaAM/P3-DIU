d9e65f6c2cf25f188586954a57c5052a2b1f2712
Esther <esther.zurita101@alu.ulpgc.es>
2020-10-18 18:11:34 +0100

    Versión 1

---
+++ src/main/java/com/mycompany/practica3/diu/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.practica3.diu;
+
+/**
+ *
+ * @author Grupo 6
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        InterfazGUI interfaz = new InterfazGUI();
+        interfaz.setVisible(true);
+    }
+}

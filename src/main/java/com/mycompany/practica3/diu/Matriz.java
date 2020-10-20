632f4fe3d32ff17cad45f8e6a09a9a6e5429fd4d
Esther <esther.zurita101@alu.ulpgc.es>
2020-10-20 23:21:01 +0100

    Versión 2

--- src/main/java/com/mycompany/practica3/diu/Matriz.java
+++ src/main/java/com/mycompany/practica3/diu/Matriz.java
@@ -52,7 +52,7 @@ public class Matriz {
         Random random = new Random();
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = random.nextInt(getMax() + 1 - getMin()) + getMin();
+                matrix[i][j] = random.nextInt(max + 1 - min) + min;
             }
         }
     }
